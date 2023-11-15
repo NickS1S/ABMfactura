bbf914168fdacc217d8fc61308dbcd20dcf3aa13
NickS1S <nicolasmaximiliano020202@gmail.com>
2023-11-15 13:04:08 -0300

    Intento1

---
+++ src/services/FacturaService.ts
@@ -0,0 +1,50 @@
+import { Factura } from "../types/Factura";
+
+const BASE_URL = 'http://localhost:8080/api/v1';
+
+export const FacturaService = {
+
+    getFacturas: async (): Promise<Factura[]> => {
+        const response = await fetch(`${BASE_URL}/facturas/getAll`);
+        const data = await response.json();
+
+        return data;
+    },
+
+    getFactura: async (id: number): Promise<Factura> => {
+        const response = await fetch(`${BASE_URL}/facturas/${id}`);
+        const data = await response.json();
+        return data;
+    },
+
+    createFactura: async (factura: Factura): Promise<Factura> => {
+        const response = await fetch(`${BASE_URL}/facturas`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(factura)
+        });
+        const data = await response.json();
+        return data;
+    },
+
+    updateFactura: async (id: number, factura: Factura): Promise<Factura> => {
+        const response = await fetch(`${BASE_URL}/facturas/${id}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(factura)
+        });
+        const data = await response.json();
+        return data;
+    },
+
+    deleteFactura: async (id: number): Promise<void> => {
+        await fetch(`${BASE_URL}/facturas/${id}`, {
+            method: "DELETE"
+        });
+
+    }
+}
